6b7e686e05471acee1bea96d4c243c7411d0fdea
ngoctuhan <48152937+ngoctuhan@users.noreply.github.com>
2019-08-03 21:58:42 +0700

    xoa project nhom5, them lai project gallery

---
+++ Gallery/app/src/main/java/com/example/gallery/adapter/TabLayoutAdapter.java
@@ -0,0 +1,48 @@
+package com.example.gallery.adapter;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.gallery.fragment.AlbumFragment;
+import com.example.gallery.fragment.MemoryFragment;
+import com.example.gallery.fragment.PhotoFragment;
+
+public class TabLayoutAdapter extends FragmentPagerAdapter {
+
+    private Context myContext;
+    int totalTabs;
+    public TabLayoutAdapter(Context context, FragmentManager fm, int totalTabs) {
+        super(fm);
+        myContext = context;
+        this.totalTabs = totalTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                PhotoFragment photoFragment = new PhotoFragment();
+                return photoFragment;
+
+            case 1:
+                AlbumFragment albumFragment = new AlbumFragment();
+                return albumFragment;
+            case 2:
+
+                MemoryFragment memoryFragment = new MemoryFragment();
+                return memoryFragment;
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+
+        return totalTabs;
+    }
+}
---
+++ Gallery/app/src/main/java/com/example/gallery/fragment/PhotoFragment.java
@@ -0,0 +1,22 @@
+package com.example.gallery.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.gallery.R;
+
+public class PhotoFragment extends Fragment {
+
+    public PhotoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.photo_fragment, container, false);
+    }
+}
