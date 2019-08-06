e9b89551d25806edbf470ce0214ec18255ee27f1
NPLinhUET <lumerijirinmo@gmail.com>
2019-08-07 01:23:33 +0700

    no message

--- Gallery/app/src/main/java/com/example/gallery/adapter/PhotoAdapter.java
+++ Gallery/app/src/main/java/com/example/gallery/adapter/PhotoAdapter.java
@@ -1,6 +1,8 @@
 package com.example.gallery.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +20,28 @@ import java.util.List;
 public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoHolder> {
     private List<Photo> photos;
     private Context context;
+    private int photo_width;
 
     public PhotoAdapter(List<Photo> photos, Context context) {
         this.photos = photos;
         this.context = context;
-    }
+        int iDisplayWidth = context.getResources().getDisplayMetrics().widthPixels;
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float dp = iDisplayWidth / (metrics.densityDpi / 160f);
 
+        if (dp < 360) {
+            dp = (dp - 17) / 2;
+            float px = convertDpToPixel(dp, context);
+            photo_width = Math.round(px);
+        }
+    }
+    public static float convertDpToPixel(float dp, Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return px;
+    }
     @NonNull
     @Override
     public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -37,6 +55,9 @@ public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.PhotoHolder>
         Glide.with(context)
                 .load(photo.getPath())
                 .into(holder.photo);
+        holder.photo.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        holder.photo.setMaxHeight(photo_width);
+        holder.photo.setMaxWidth(photo_width);
         holder.title.setText(photo.getTitle());
     }
 
